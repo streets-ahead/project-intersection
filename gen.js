d66172f0c51fa20715702d39395ec265e3238de7
Sam Mussell <sam@salabs.io>
2016-02-06 23:49:54 -0800

    add error message

--- gen.js
+++ gen.js
@@ -2,30 +2,33 @@ require("babel-register");
 var glob = require('glob');
 var fs = require('fs-extra');
 var axios = require('axios');
-var routeConfig = require('./src/routeConfig');
+var routes = require('./src/routeConfig').routes;
 var endsWith = require('lodash/endsWith');
 
 var URL = "http://localhost:3000";
 
-const dirs = routeConfig.routes[0].childRoutes.map(r => r.path);
-dirs.forEach((d) => fs.mkdirsSync(__dirname + '/dist/' + d));
+axios.head(URL).then(generate, (e) => console.error("[ERROR] The server must be running when using the generator"));
 
-const files = glob.sync(`./@(${dirs.join('|')})/*.md`);
-files.forEach((f) => {
-  const file = f.replace(/(^\.\/)|(\.md$)/ig, '');
-  saveFile(file + '.html');
-  saveFile(file + '.json');
-});
+function generate() {
+  const dirs = routes[0].childRoutes.map(r => r.path);
+  dirs.forEach((d) => fs.mkdirsSync(__dirname + '/dist/' + d));
 
-saveFile('index.json');
-saveFile('', 'index.html');
-
-function saveFile(file, output) {
-  console.info('SAVING: ', file);
-  axios.get(URL + '/' + file).then((result) => {
-    fs.writeFile('dist/' + (output || file), 
-                endsWith(file, '.json') ? JSON.stringify(result.data) : result.data, 
-                () => {});
+  const files = glob.sync(`./@(${dirs.join('|')})/*.md`);
+  files.forEach((f) => {
+    const file = f.replace(/(^\.\/)|(\.md$)/ig, '');
+    saveFile(file + '.html');
+    saveFile(file + '.json');
   });
-  
+
+  saveFile('index.json');
+  saveFile('', 'index.html');
+
+  function saveFile(file, output) {
+    console.info('SAVING: ', file);
+    axios.get(URL + '/' + file).then((result) => {
+      fs.writeFile('dist/' + (output || file), 
+                  endsWith(file, '.json') ? JSON.stringify(result.data) : result.data, 
+                  () => {});
+    });  
+  }
 }
