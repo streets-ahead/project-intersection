f00c6a24492197943a56945f954d653c179e828d
Sam Mussell <sam@salabs.io>
2016-03-12 13:05:26 -0800

    tweak animation

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -43,12 +43,12 @@ export default function Post(props) {
                         ))}>
           {values => (
             <div className={styles['header-content']}>
-              <ul style={{transform: `translateZ(${values[0].t}px)`}} className={styles['tags-box']}>
+              <ul style={{transform: `translateZ(${values[2].t}px)`}} className={styles['tags-box']}>
                 {tags.map(d => <li key={d}>{d}</li>)}
               </ul>
               <h1 style={{transform: `translateZ(${values[1].t}px)`}}>{title}</h1>
-              <p style={{transform: `translateZ(${values[2].t}px)`}} className={styles['subhead']}>{subHead}</p>
-              <p style={{transform: `translateZ(${values[3].t}px)`}} className={styles['date']}>
+              <p style={{transform: `translateZ(${values[3].t}px)`}} className={styles['subhead']}>{subHead}</p>
+              <p style={{transform: `translateZ(${values[0].t}px)`}} className={styles['date']}>
                 {dateFormat(new Date(published), "mmm dd, yyyy")} / {author}
               </p>
             </div>
