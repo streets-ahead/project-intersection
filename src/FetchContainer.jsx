adec018b200f3ebe49a632411426d9720ed1b8ca
Sam Mussell <sam@salabs.io>
2016-03-15 23:33:13 -0700

    fix ios scrolling issue

--- src/FetchContainer.jsx
+++ src/FetchContainer.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import api from './api';
 
+let isIOS = false;
+
+if(typeof window !== "undefined") {
+  isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+}
+
 export default function(Component, type) {
   class FetchContainer extends React.Component {
     componentWillMount() {
-      this.updateSize = () => this.forceUpdate();
+      this.updateSize = () => {
+        const forceUpdate = !isIOS || (isIOS && this.__width !== window.innerWidth);
+        if(forceUpdate) this.forceUpdate();
+      console.log('qwerqwer', isIOS, this.__width, window.innerWidth)
+        this.__width = window.innerWidth;
+      }
+      
+      this.__width = window.innerWidth;
       if(typeof window !== "undefined") {
         window.addEventListener('resize', this.updateSize);
       }
